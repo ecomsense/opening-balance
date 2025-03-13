03c53eb0299cca20f05fae5b3df0dd71354264e5
SuperAIAlgo <harinathnl@gmail.com>
2025-03-13 17:34:57 +0530

    first commit

--- src/entry.py
+++ src/entry.py
@@ -71,3 +71,12 @@ capital 1L
 pfolio_stop 2%
 pfolio_target 5%
 """
+from constants import O_SETG
+# pprint is optional .. it pretty prints
+# complex data types
+from pprint import pprint
+
+pprint(O_SETG)
+keys = ["log", "targets", "trade", "MCX"]
+four_key_items = {key: value for key, value in O_SETG.items() if isinstance(value, dict) and key not in keys}
+print(f"{four_key_items=}")
