bd7d9b556f41ad6d4bbfb90bd360408f5197d797
pannet1 <prog@ecomsense.in>
2025-03-15 10:49:57 +0530

    rm Entry

--- src/Entry.py
+++
@@ -1,9 +0,0 @@
-from constants import O_SETG
-# pprint is optional .. it pretty prints
-# complex data types
-from pprint import pprint
-
-pprint(O_SETG)
-keys = ["log", "targets", "trade", "MCX"]
-four_key_items = {key: value for key, value in O_SETG.items() if isinstance(value, dict) and key not in keys}
-print(f"{four_key_items=}")
